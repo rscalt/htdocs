cb75b7089e6e438b6bee0ce1293653b85dca1c03
rscugh <asgardrowizard@yandex.ru>
2021-03-28 17:52:15 +0500

    ex13_final

--- php/php_test/ex13.php
+++ php/php_test/ex13.php
@@ -1,15 +1,7 @@
-<!-- 
-    13. Создайте массив продуктов с параметрами id, price, name. 
-    Выведите каждый товар в виде "id. name - price". 
-    После каждого добавьте форму с кнопкой купить. 
-    Сделать обработку этой кнопки с добавлением товара в корзину. 
-    Сделать через сессию. 
-    При повторном нажатие, если товар уже в корзине, то его количество должно увеличиваться на 1. 
-    Снизу сделать вывод, что уже есть в корзине. 
--->
-
 <?php //куки и сессии
 session_start();
+checkPrevSession();
+//$_SESSION = []; //отладка
 ?>
 
 <?php //классы
@@ -19,87 +11,152 @@ class Product
     protected $price;
     protected $name;
 
-    public function showProduct()
+    //возвращает все поля продукта
+    public function ShowProduct()
     {
-        print "\n";
         print "$this->id. ";
         print "$this->name - ";
         print "$this->price";
     }
 
-    protected static function getID(): int //абстрактный метод
+    //возвращает ID конкретного продукта
+    public function GetProductID()
+    {
+        print "$this->id";
+    }
+
+    //возвращает ID для присвоения новому продукту
+    protected static function GetNewID(): int //для метода-конструктора
     {
         static $last_id = 1; //сохраняем значение ID между вызовами
         $id = $last_id++;
         return $id;
     }
 
-    //метод-конструктор
-    public static function createProduct(
+    //возвращает новый продукт с заданными параметрами
+    public static function CreateProduct(
         string $new_name,
         float $new_price
     ): Product {
         $instance = new Product;
         if (isset($instance)) {
             //увеличиваем id только если объект создан успешно
-            $new_id = Product::getID();
+            $new_id = Product::GetNewID(); //присваиваем новый уникальный ID
             $instance->id = $new_id;
             $instance->name = $new_name;
             $instance->price = $new_price;
             return $instance;
-        } else return 0;
+        } else return -1;
     }
 }
 ?>
 
 <?php //функции
-function print_prod_db(array $prod_arr)
+
+//возвращает результат проверки на наличие сохраненных сессий
+//если сохраненных сессий нет - формирует массивы 'id' и 'q' в $_SESSION
+function checkPrevSession()
 {
-    //выводим список продуктов по переданному массиву-корзине
+    $prev_session_exists = isset($_SESSION['id']) && isset($_SESSION['q']);
+    if (!$prev_session_exists) {
+        //print "No previous session found! <br>"; //отладка
+        //print "Session have been initialized!"; //отладка
+        $_SESSION['id'] = [];
+        $_SESSION['q'] = []; //количество
+        return false;
+    } else return true;
+}
+
+
+//возвращает список доступных продуктов из массива
+function printProdDB(array $prod_arr)
+{
     foreach ($prod_arr as $group => $object_array) {
         foreach ($object_array as $object)
-            $object->showProduct(); //10of10
+            $object->ShowProduct(); //10of10
     }
 }
 
+//добавляет товар в корзину (реализация - через сессию)
+function addToBasket($post_id)
+{
+    $id_exists = in_array($post_id, $_SESSION['id'], true); //есть ли в корзине
+
+    if ($id_exists) { //если есть
+        $id_pos = array_search($post_id, $_SESSION['id'], null); //то по индексу
+        $_SESSION['q'][$id_pos]++; //...увеличиваем на 1; (массивы параллельные)
+        return $_SERVER['PHP_SELF']; //обновляем страницу
+    } else { //иначе
+        $_SESSION['id'][] = $post_id; //добавляем id в конец массива
+        $_SESSION['q'][] = 1; //добавляем в конец массива; код более наглядный
+        return $_SERVER['PHP_SELF']; //обновляем страницу
+    }
+}
 ?>
 
-<?php //запросы к БД
+<?php //данные и обработка запросов
+//данные из ""БД""
 $prod_db = array(
     'bread' => array(
-        Product::createProduct('bread1', 11),
-        Product::createProduct('bread2', 22),
+        Product::CreateProduct('bread1', 11),
+        Product::CreateProduct('bread2', 22),
     ),
 
     'milk'  => array(
-        Product::createProduct('milk1', 33),
-        Product::createProduct('milk2', 44),
-        Product::createProduct('milk3', 55)
+        Product::CreateProduct('milk1', 33),
+        Product::CreateProduct('milk2', 44),
+        Product::CreateProduct('milk3', 55)
     ),
 
     'sugar' => array(
-        Product::createProduct('sugar1', 66),
+        Product::CreateProduct('sugar1', 66),
     ),
 );
+//обработка запросов
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_prod_id'])) {
+    addToBasket($_POST['add_prod_id']); //добавляем в корзину по $_POST[]
+} else $_SERVER['PHP_SELF'];
 ?>
 
-<?php //контент
-foreach ($prod_db as $group => $object_array) :
-    foreach ($object_array as $object) :
+
+<?php //контент: витрина 
 ?>
-        <table>
+<h1>Витрина</h1>
+<table border="1">
+    <th>ID, наим., цена</th>
+    <th>Купить</th>
+    <?php foreach ($prod_db as $group => $object_array) : ?>
+        <?php foreach ($object_array as $object) : ?>
             <tr>
                 <td>
-                    <?php $object->showProduct(); ?>
+                    <?php $object->ShowProduct(); //вывод объектов и кнопки 
+                    ?>
                 </td>
                 <td>
-                    <form action="$_SERVER[PHP_SELF]" method="POST">
-                        <input type="submit" name="product_id" value="Купить">
+                    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST">
+                        <button name="add_prod_id" type="submit" value="<?= $object->GetProductID() ?>">
+                            <b>В корзину</b>
+                        </button>
                     </form>
                 </td>
             </tr>
-        </table>
-<?php
-    endforeach;
-endforeach;
+        <?php endforeach; ?>
+    <?php endforeach; ?>
+</table>
+
+
+<?php //контент: корзина 
 ?>
+<h1>Корзина</h1>
+<table border="1">
+    <th>№</th>
+    <th>ID</th>
+    <th>Кол-во</th>
+    <?php foreach ($_SESSION['id'] as $position => $value) : ?>
+        <tr>
+            <td><?= ($position + 1); ?></td>
+            <td><?= $_SESSION['id'][$position]; ?></td>
+            <td><?= $_SESSION['q'][$position]; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
